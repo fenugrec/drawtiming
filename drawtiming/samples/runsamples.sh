c87b42e37baf55ddf37fb78d44cb93edecdaa4c1
Edward Counce <ecounce@users.sourceforge.net>
2004-09-24 16:32:34 +0000

    turnup sample doesn't demonstrate anything unique
    svn path=/trunk/; revision=36

--- drawtiming/samples/runsamples.sh
+++ drawtiming/samples/runsamples.sh
@@ -2,6 +2,5 @@
 # this script is meant to be executed by running "make check" 
 
 ../src/drawtiming -o sample.gif $srcdir/sample.txt
-../src/drawtiming -o turnup.gif $srcdir/turnup.txt
 ../src/drawtiming -o statement1.gif $srcdir/statement1.txt
 ../src/drawtiming -x 1.5 -o memory.gif $srcdir/memory.txt
